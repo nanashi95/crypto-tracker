e118fa387d030bfa96d9a760347d3703b3f488d8
nanashi95 <lekayser@yahoo.fr>
2020-02-04 07:38:30 +0900

    add assets icons

---
+++ lib/images/assets_icons/NAMECHANGE.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+for file in * ; do            # or *.jpg, or x*.jpg, or whatever
+   basename=$(tr '[:lower:]' '[:upper:]' <<< "${file%.*}")
+   newname="$basename.${file#*.}"
+   mv "$file" "$newname"
+done
